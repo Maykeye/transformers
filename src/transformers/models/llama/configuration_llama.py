1e61ccd6a518db240df5e6af9be08fbf7f9aac1e
Maykeye <Maykeye@users.noreply.github.com>
2023-06-29 12:20:54 +0600

    [llama] Make upcast optional

--- src/transformers/models/llama/configuration_llama.py
+++ src/transformers/models/llama/configuration_llama.py
@@ -97,6 +97,7 @@ class LlamaConfig(PretrainedConfig):
         bos_token_id=1,
         eos_token_id=2,
         tie_word_embeddings=False,
+        use_attn_upcast=True,
         **kwargs,
     ):
         self.vocab_size = vocab_size
@@ -109,6 +110,7 @@ class LlamaConfig(PretrainedConfig):
         self.initializer_range = initializer_range
         self.rms_norm_eps = rms_norm_eps
         self.use_cache = use_cache
+        self.use_attn_upcast = use_attn_upcast
         super().__init__(
             pad_token_id=pad_token_id,
             bos_token_id=bos_token_id,
--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -228,8 +228,12 @@ class LlamaAttention(nn.Module):
                 attn_weights, torch.tensor(torch.finfo(attn_weights.dtype).min, device=attn_weights.device)
             )
 
-        # upcast attention to fp32
-        attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+        # optionally upcast attention to fp32
+        if self.config.use_attn_upcast:
+            attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+        else:
+            attn_weights = nn.functional.softmax(attn_weights, dim=-1).to(query_states.dtype)
+
         attn_output = torch.matmul(attn_weights, value_states)
 
         if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
